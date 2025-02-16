eddafebf647f1b378b3099e8a5f3ddbacd793ac2
rifushigi <tope.dansa@gmail.com>
2025-02-16 16:59:42 +0100

    add update and delete routes to food API

--- src/routes/food.ts
+++ src/routes/food.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { searchFood, getFoodById, getAllFoods, addFood } from '../controllers/foodController';
+import { searchFood, getFoodById, getAllFoods, addFood, updateFood, deleteFood } from '../controllers/foodController';
 // import { auth } from '../middleware/auth';
 
 const router = express.Router();
@@ -8,5 +8,7 @@ router.get('/search', searchFood);
 router.get('/:id', getFoodById);
 router.get('/', getAllFoods);
 router.post('/', addFood);
+router.put('/:id', updateFood);
+router.delete('/:id', deleteFood);
 
 export default router;
