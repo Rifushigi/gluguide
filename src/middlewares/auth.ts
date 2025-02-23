e4fde3ede772dc93a91738995b27027a9b69ad13
Rifushigi <tope.dansa@gmail.com>
2025-02-23 15:57:15 +0100

    add authentication middleware to handle JWT verification

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,32 @@
+import { JWTError } from '../types/errors';
+import { asyncHandler } from '@/utils/asyncHandler';
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+interface JwtPayload {
+    userId: string;
+}
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: JwtPayload;
+        }
+    }
+}
+
+export const authMiddleware = asyncHandler(async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+        const token = req.header('Authorization')?.replace('Bearer ', '');
+
+        if (!token) throw new JWTError('No token provided');
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload;
+
+        if (!decoded) throw new JWTError('Invalid token');
+        req.user = decoded;
+        next();
+});
