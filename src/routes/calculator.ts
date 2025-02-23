efd3862695fe358d029a71c5031b8c35cd3876d3
Rifushigi <tope.dansa@gmail.com>
2025-02-23 15:56:09 +0100

    remove calculator controller

--- src/routes/calculator.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { calculateCalories } from '../controllers/calculatorController';
-
-const router = express.Router();
-
-router.post('/', calculateCalories);
-
-export default router;
