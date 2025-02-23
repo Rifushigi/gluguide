adaf02ae0799ab1c00ba362a78decd2c01afc677
Rifushigi <tope.dansa@gmail.com>
2025-02-23 15:55:24 +0100

    add dietary, activity, and user routes to app

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDatabase } from './config/database';
 import foodRoutes from './routes/food';
-import calculatorRoutes from './routes/calculator';
-import { errorLogger } from './middlewares/errorLogger'
+import dietaryRoutes from './routes/dietary';
+import activityRoutes from './routes/activity';
+import userRoutes from './routes/user';
+import { errorLogger } from './middlewares/errorLogger';
 import { requestLogger } from './middlewares/requestLogger';
 import { seedDatabase } from './config/seedDatabase';
 import { errorHandler } from './middlewares/errorHandler';
@@ -19,16 +21,18 @@ app.use(requestLogger);
 
 // Routes
 app.use('/api/foods', foodRoutes);
-app.use('/api/calculator', calculatorRoutes);
+app.use('/api/dietary', dietaryRoutes);
+app.use('/api/activities', activityRoutes);
+app.use('/api/users', userRoutes);
 
 // Connect to database
 connectDatabase();
 
-// seed database
+// Seed database
 seedDatabase();
 
 app.use(errorLogger);
-app.use(errorHandler)
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
