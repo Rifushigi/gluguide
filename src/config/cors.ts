166011bd51f80cc039f98fd6bce16bce67883b7a
Rifushigi <tope.dansa@gmail.com>
2025-02-24 12:16:14 +0100

    feat: enable credentials in CORS options for improved security with cross-origin requests

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,5 +1,6 @@
 export const corsOptions = {
     origin: ['http://localhost:3000', 'http://127.0.0.1:3000', '*'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
 };
