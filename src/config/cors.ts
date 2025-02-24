9391f00dea3536682fef0b4cbf3edf676b3926a5
Rifushigi <tope.dansa@gmail.com>
2025-02-24 12:27:48 +0100

    feat: simplify CORS options to allow all origins for broader access

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,5 +1,5 @@
 export const corsOptions = {
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', '*'],
+    origin: ['*'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
