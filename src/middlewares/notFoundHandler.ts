2a8d89a6ef6655f90801e6f9f63291bfa82525de
Rifushigi <tope.dansa@gmail.com>
2025-02-24 07:16:50 +0100

    fix: enhance not found error message to include requested URL

--- src/middlewares/notFoundHandler.ts
+++ src/middlewares/notFoundHandler.ts
@@ -2,6 +2,6 @@ import { Request, Response, NextFunction } from 'express';
 import { NotFoundError } from '../types/errors';
 
 export const notFoundHandler = (req: Request, res: Response, next: NextFunction) => {
-    const error = new NotFoundError('Route not found');
+    const error = new NotFoundError('Route [' + req.url+']');
     next(error);
 };
