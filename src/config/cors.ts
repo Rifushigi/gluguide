62daab9153595a19d121d17810c4ad6fa3f6e55d
Omosuyi Olawole <omooladev@gmail.com>
2025-05-04 17:31:33 +0100

    updated the live domain on the cors options origin

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,5 +1,5 @@
 export const corsOptions = {
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://glu-guide.vercel.app'],
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://glue-guide.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
